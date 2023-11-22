56e2d6d871d22f78603697492e05940c3d6237f3
ShiryaeP <shiryaep@gmail.com>
2023-11-22 20:49:03 +0300

    Инициализирующий коммит + на home добавлены 4 карточки с кнопками

---
+++ PizzaPro/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PizzaPro;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
